2e94b6b70662d101644e3b7c8cd8922e7cc20025
shr000my <ahahriot@gmail.com>
2021-02-21 14:56:15 +0200

    test deploy endpoint

--- api/app.js
+++ api/app.js
@@ -31,6 +31,10 @@ app.use(function (req, res, next) {
   next(createError(404));
 });
 
+app.get("/deploy", (req, res) => {
+  res.send("Success");
+});
+
 // error handler
 app.use(function (err, req, res, next) {
   // set locals, only providing error in development
